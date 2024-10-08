7dc952b41565eee917506da06c6b5e6d6a6ac878
Krishna Kant <sharmakrishnakant721@gmail.com>
2024-10-08 19:07:51 +0530

    add

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 //connect to database
 exports.connect  = ()=>{
     mongoose.connect(process.env.MONGODB_URL,{
-    
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     }).then(()=>console.log('Connected to database'))
         .catch(err=>console.error('Error connecting to database:',err));
 }
